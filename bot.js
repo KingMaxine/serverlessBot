2b1b49ab5e6c66bdd6734deb4aeaa131620e549c
slowworks <53411077+slowworks@users.noreply.github.com>
2021-05-24 11:38:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const { Telegraf } = require('telegraf');
 const bot = new Telegraf('1806769124:AAEA3ocdTDN0to7wXWFulm4N3bVuwZBrpeY')
-const url = 'https://serverless-bot.vercel.app'
 
-
+app.use(bodyParser.json())
 
 bot.telegram.deleteWebhook().then(success => {
   success && console.log('🤖 is listening to your commands')
 })
 
-
-
-bot.telegram.setWebhook('https://serverless-bot.vercel.app/secretPath')
-
 bot.start(async(ctx)=> {
 ctx.reply('hello')
 })
 
-
-
-app.use(bodyParser.json());
+bot.telegram.setWebhook('https://serverless-bot.vercel.app/secretPath')
 
 app.use(bot.webhookCallback('/secretPath'))
-
 
 
 app.get('/', (req, res) => res.send('Home Page Route'));
@@ -37,6 +29,5 @@ app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
-// Set the bot API endpoint
 
 
