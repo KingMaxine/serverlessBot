211cb964d0cd1de77ad8d6653470de21bb111767
slowworks <53411077+slowworks@users.noreply.github.com>
2021-05-24 11:04:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 3000;
-const { Telegraf, session, Extra, Markup, Scenes} = require('telegraf');
-const { BaseScene, Stage } = Scenes
+const port = process.env.PORT || 8080;
+const { Telegraf } = require('telegraf');
 const bot = new Telegraf('1806769124:AAEA3ocdTDN0to7wXWFulm4N3bVuwZBrpeY')
-const {enter, leave} = Stage
-const stage = new Stage();
-const Scene = BaseScene
-bot.use(session())
-bot.use(stage.middleware())
-const url = 'https://serverless-bot.vercel.app/'
+const url = 'https://serverless-bot.vercel.app'
 
 
 
@@ -20,12 +14,17 @@ bot.telegram.deleteWebhook().then(success => {
 
 
 
-//bot.telegram.setWebhook(url+'/secretpath')
+bot.telegram.setWebhook(url+'/secretpath')
+
+bot.start(async(ctx)=> {
+ctx.reply('hello')
+})
+
 
 
 app.use(bodyParser.json());
 
-//app.use(bot.webhookCallback('/secret-path'))
+app.use(bot.webhookCallback('/secret-path'))
 
 
 
@@ -35,32 +34,9 @@ app.get('/about', (req, res) => res.send('About Page Route'));
 
 app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
 
-
-/* Error handler middleware */
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  console.error(err.message, err.stack);
-  res.status(statusCode).json({'message': err.message});
-  
-  return;
-});
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
-
-
-
-
-
-
-
-bot.start(async(ctx)=> {
-ctx.reply('hello')
-})
-
-bot.launch()
-
 // Set the bot API endpoint
 
 
