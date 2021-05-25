6b764aa5fe8f7fe80ad16c48ac3aa018cdab3d7b
slowworks <53411077+slowworks@users.noreply.github.com>
2021-05-25 14:53:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,9 +11,6 @@ app.get('/about', (req, res) => res.send('About Page Route'));
 
 app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-});
 
 module.exports= app
 
